fb70dd79e297c193e7ebe44cf9d2021c1f64b401
pavankumarallu <vamsipavankumarallu@gmail.com>
2022-05-03 17:58:20 +0530

    Second Commit

--- fun_dbms.py
+++ fun_dbms.py
@@ -1,35 +1,51 @@
 import mysql.connector
 
-def dashboard_faculty():
-	cursor=connection.cursor()
-	cursor.execute("SELECT * from faculty_member where faculty_id='"+request.user.username+"';")
-	#data = cursor.fetchone()
-	cursor.execute("SELECT leave_type, no_days_left from days_left where faculty_id='"+data[0]+"';")
-	#days = cursor.fetchone()
-	connection.close()
+# def dashboard_faculty():
+# 	cursor=connection.cursor()
+# 	cursor.execute("SELECT * from faculty_member where faculty_id='"+request.user.username+"';")
+# 	#data = cursor.fetchone()
+# 	cursor.execute("SELECT leave_type, no_days_left from days_left where faculty_id='"+data[0]+"';")
+# 	#days = cursor.fetchone()
+# 	connection.close()
 	
-	return(data, days)
+# 	return(data, days)
 
-# We will show only approved leaves to the faculty	
-def history_page():
-	cursor=connection.cursor()
-	cursor.execute("SELECT * from leave_checked where faculty_id='"+request.user.username+"';")
+# # We will show only approved leaves to the faculty	
+# def history_page():
+# 	cursor=connection.cursor()
+# 	cursor.execute("SELECT * from leave_checked where faculty_id='"+request.user.username+"';")
 	
 
 
-def admin_hod():
-	cursor=connection.cursor()
-	cursor.execute("SELECT * from leave_request where leave_type=Casual,Special Casual, Restricted;")
+# def admin_hod():
+#     cursor=connection.cursor()
+# 	cursor.execute("SELECT * from leave_request where leave_type=Casual,Special Casual, Restricted;")
 
-def admin_director():
-	cursor=connection.cursor()
-	cursor.execute("SELECT * from leave_request where leave_type=Leave Not Due,Extraordinary, Maternity,Quarantine, Child Care, Sabbatical, FST;")
+# def admin_director():
+# 	cursor=connection.cursor()
+# 	cursor.execute("SELECT * from leave_request where leave_type=Leave Not Due,Extraordinary, Maternity,Quarantine, Child Care, Sabbatical, FST;")
 
-def admin_DOFA():
-	cursor=connection.cursor()
-	cursor.execute("SELECT * from leave_request where leave_type=Earned,Vacation, Half Pay, Commuted, Paternity;")
+# def admin_DOFA():
+# 	cursor=connection.cursor()
+# 	cursor.execute("SELECT * from leave_request where leave_type=Earned,Vacation, Half Pay, Commuted, Paternity;")
 
 
+class Database:
+    def __init__(self):
+        conn = mysql.connector.connect(
+            					user = "root",
+                                host = "127.0.0.1",
+                                password = "{Poorna@01}",
+                                database = "taxmanagementsystem",
+                                auth_plugin = 'mysql_native_password')
+        
+    def Register(self,FirstName,LastName,Age):
+        mycursor = self.conn.cursor()
+        sql = "INSERT INTO reg_details (FirstName,LastName,Age) VALUES (%s,%s,%s)"
+        val = (FirstName,LastName,Age)
+		mycursor.execute(sql,val)
+		return True
+    
 
 
 	
--- leave_req.py
+++ leave_req.py
@@ -5,20 +5,13 @@ from PySide2.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTi
 
 from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
 from PySide2.QtWidgets import *
-from fun_dbms import Database
-from UI_Frontend.register_page import Ui_Register_page
-class Register_window:
+
+from UI_Frontend.leave_request import Ui_Leave_request_page
+class leave_request_window:
     def __init__(self):
         self.main_win = QMainWindow()
-        self.ui = Ui_Register_page()
+        self.ui = Ui_Leave_request_page()
         self.ui.setupUi(self.main_win)
         
-        self.ui.Register_btn.clicked.connect(self.Register)
-    
-    def Register(self):
-        self.db = Database()
-        msg = self.db.Register(self.ui.fac_name_edt.text(),)
-        
-      
     def show(self):
         self.main_win.show()
